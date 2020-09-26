0832f4c5564a95b51ab92827218756e93e37f873
Resul Çelik <resul_celik@hotmail.com>
2020-09-26 11:06:37 +0200

    *Initial Commit*

---
+++ DiscordBot.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace WpfApp1
+{
+	class DiscordBot
+	{
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		private DiscordSocketClient m_client;
+		private SocketVoiceChannel m_listenTo = null;
+		private ISocketMessageChannel m_textChannel = null;
+
+		public async void Start(string _token)
+		{
+			m_client = new DiscordSocketClient();
+
+			m_client.Log += Log;
+
+			//  You can assign your bot token to a string, and pass that in to connect.
+			//  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+			var token = _token;
+
+			// Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+			// var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+			// var token = File.ReadAllText("token.txt");
+			// var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+			await m_client.LoginAsync(TokenType.Bot, token);
+			await m_client.StartAsync();
+
+			m_client.MessageReceived += ClientOnMessageReceived;
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+
+		public void Mute()
+		{
+			if (m_listenTo == null)
+			{
+				return;
+			}
+
+			foreach (var user in m_listenTo.Users)
+			{
+				user.ModifyAsync((a) => { a.Mute = true; });
+			}
+
+			m_textChannel.SendMessageAsync("Muting");
+		}
+
+		public void UnMute()
+		{
+			if (m_listenTo == null)
+			{
+				return;
+			}
+
+			foreach (var user in m_listenTo.Users)
+			{
+				user.ModifyAsync((a) => { a.Mute = false; });
+			}
+
+			m_textChannel.SendMessageAsync("UnMuting");
+		}
+
+		private Task ClientOnMessageReceived(SocketMessage arg)
+		{
+			if (arg.Content.StartsWith("!StartGame"))
+			{
+				foreach (SocketGuild guild in m_client.Guilds)
+				{
+
+					foreach (var voice in guild.VoiceChannels)
+					{
+						if (voice.Users.FirstOrDefault((x) => { return string.Compare(x.AvatarId, arg.Author.AvatarId, StringComparison.InvariantCultureIgnoreCase) == 0; }) != null)
+						{
+							m_listenTo = voice;
+							arg.Channel.SendMessageAsync("Muting everyone in " + m_listenTo.Name);
+							m_textChannel = arg.Channel;
+							Mute();
+							break;
+						}
+					}
+				}
+			}
+
+			if (arg.Content.StartsWith("!EndGame"))
+			{
+				arg.Channel.SendMessageAsync("Unmuting everyone in " + m_listenTo.Name);
+				UnMute();
+				m_listenTo = null;
+				m_textChannel = null;
+
+			}
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Discord;
+using Memory;
+
+namespace WpfApp1
+{
+	public partial class MainWindow : Window
+	{
+		public bool inMeeting = false;
+		public Thread Thread = null;
+
+		public Mem MemLib = new Mem();
+		private DiscordBot bot;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var isOpen = MemLib.OpenProcess("Among Us.exe");
+
+			if (isOpen)
+			{
+				var posX = MemLib.ReadFloat("UnityPlayer.dll+01277F00,0x20,0x2C,0x58,0x0,0x4,0x5C,0x2C");
+
+				Debug.WriteLine(posX);
+
+
+				Thread = new Thread(new ThreadStart(ThreadProc));
+				Thread.Start();
+
+				bot = new DiscordBot();
+			}
+			else
+			{
+				Debug.WriteLine("Could not Open Among us");
+			}
+		}
+
+		private void ThreadProc()
+		{
+
+			var inEmergencyMeeting = MemLib.ReadByte("UnityPlayer.dll+012A7A14,0x64,0x34,0x8,0xC,0x3C,0x18");
+			if (inEmergencyMeeting == 1 && !inMeeting)
+			{
+				inMeeting = true;
+				bot.UnMute();
+			}
+			else if(inEmergencyMeeting != 1 && inMeeting)
+			{
+				inMeeting = false;
+				bot.Mute();
+			}
+			Thread.Sleep(100);
+
+			Debug.WriteLine(inMeeting);
+
+			if (MemLib.theProc.HasExited)
+			{
+				bot.UnMute();
+				return;
+			}
+
+			Thread = new Thread(new ThreadStart(ThreadProc));
+			Thread.IsBackground = true;
+			Thread.Start();
+		}
+
+		private void OnApplyTokenButtonClicked(object sender, RoutedEventArgs e)
+		{
+			bot.Start(TokenBox.Text);
+		}
+	}
+
+
+}
